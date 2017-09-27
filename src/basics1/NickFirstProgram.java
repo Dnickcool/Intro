a64279165e92d0712a4ad1ae12d472118bd7f408
javaclass <javaclass@15000513cpu.fec.org>
2017-09-27 16:24:50 -0500

    Nicks loop, hello and calculator program

---
+++ src/basics1/NickFirstProgram.java
@@ -0,0 +1,10 @@
+package basics1;
+
+public class NickFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Nick's first program");
+
+	}
+
+}
---
+++ src/basics1/NicksCalculator.java
@@ -0,0 +1,18 @@
+package basics1;
+
+public class NicksCalculator {
+
+	public static void main(String[] args) {
+		System.out.println(calculateSum(2, 4));
+
+	}
+	
+	static int calculateSum(int a, int b){
+		return a + b;
+		
+	}
+	
+	//subtraction method
+	//muliply method
+
+}
